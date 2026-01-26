0f7d64a29a9c6a7301e710a011eba2861bc3b577
Samwel Njogu Mwaniki <164732133+Sanjomwa@users.noreply.github.com>
2026-01-26 06:36:39 +0000

    commit

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "google" {
+  project     = "encoded-joy-485413-k5" # your confirmed project ID
+  region      = "africa-south1"         # preferred region
+  credentials = file("${path.module}/keys/my-creds.json")
+}
