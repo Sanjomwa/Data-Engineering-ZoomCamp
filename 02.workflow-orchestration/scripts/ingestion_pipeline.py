f01dd584ed779bc609a2723a56efc646182234ab
Samwel Njogu Mwaniki <164732133+Sanjomwa@users.noreply.github.com>
2026-02-01 10:27:29 +0000

    ingestion-pipeline

--- 02.workflow-orchestration/scripts/ingestion_pipeline.py
+++ 02.workflow-orchestration/scripts/ingestion_pipeline.py
@@ -4,15 +4,15 @@ from sqlalchemy import create_engine
 
 def read_data():
     """
-    Reads local parquet and CSV files mounted via docker-compose volumes.
+    Reads parquet and CSV files mounted via docker-compose volumes.
     Adjust paths if you change volume mounts in docker-compose.yml.
     """
 
-    # Green taxi trip data (parquet)
+    # Green taxi trip data
     green_trip_df = pd.read_parquet("/temp_app/data/green_tripdata_2025-11.parquet")
     green_trip_df.columns = [c.lower() for c in green_trip_df.columns]
 
-    # Taxi zone lookup data (csv)
+    # Taxi zone lookup data
     taxi_zone_df = pd.read_csv("/temp_app/data/taxi_zone_lookup.csv")
     taxi_zone_df.columns = [c.lower() for c in taxi_zone_df.columns]
 
