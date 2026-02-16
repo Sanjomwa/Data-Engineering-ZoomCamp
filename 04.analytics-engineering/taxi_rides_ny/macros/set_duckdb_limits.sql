f6caa5ad1bf808e8736ac4ba606f849d0d701a34
Samwel Njogu Mwaniki <164732133+Sanjomwa@users.noreply.github.com>
2026-02-16 13:49:16 +0300

    Delete 04.analytics-engineering/taxi_rides_ny/macros/set_duckdb_limits.sql

--- 04.analytics-engineering/taxi_rides_ny/macros/set_duckdb_limits.sql
+++
@@ -1,5 +0,0 @@
-{% macro set_duckdb_limits() %}
-  {% do run_query("SET memory_limit = '3GB';") %}    -- adjust 2GB–4GB depending on your Codespace size
-  {% do run_query("SET threads = '2';") %}           -- usually 1–4 is plenty
-  {% do run_query("SET enable_progress_bar = true;") %}  -- nice to see progress
-{% endmacro %}
