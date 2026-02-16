dbd208c0b8ee88becffe578e0170846e94d35a59
Samwel Njogu Mwaniki <164732133+Sanjomwa@users.noreply.github.com>
2026-02-16 11:36:32 +0000

    stage fhv file

---
+++ 04.analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with fhv_data as (
+    select 
+        -- identifiers
+        {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+        dispatching_base_num,
+        affiliated_base_number,
+        
+        -- timestamps
+        cast(pickup_datetime as timestamp) as pickup_datetime,
+        cast(dropOff_datetime as timestamp) as dropoff_datetime,
+        
+        -- trip info
+        cast(PUlocationID as integer) as pickup_locationid,
+        cast(DOlocationID as integer) as dropoff_locationid,
+        cast(SR_Flag as integer) as sr_flag
+        
+    from {{ source('nytaxi', 'fhv_tripdata') }}
+    
+    -- Filter out records where dispatching_base_num is NULL
+    where dispatching_base_num is not null
+)
+
+select * from fhv_data
+
+-- dbt build --select <model_name> --vars '{"is_test_run": false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
